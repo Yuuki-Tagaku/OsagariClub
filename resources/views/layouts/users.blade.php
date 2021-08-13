3bb21b28356f916481137ead02cab85502624208
tokie-inomata <asahane06@gmail.com>
2021-08-13 15:42:12 +0900

    CSSの修正版とUserController作成とConstファイル作成

--- resources/views/layouts/users.blade.php
+++ resources/views/layouts/users.blade.php
@@ -2,9 +2,10 @@
 <html lang="ja">
   <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
     <title>@yield('title')</title>
 
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/stylesheet.css') }}" media="screen and (max-width: 480px)">
     @yield('css')
 
     @yield('js')
@@ -13,12 +14,12 @@
     <header>
       <div class="header-container">
         <div class="header-container-logo">
-          <img src="">
+          <img src="/">
         </div>
         <div class="header-container-menu">
-          <button></button>
-          <button></button>
-          <button></button>
+          <button>おさがり<br>登録</button>
+          <button>おさがり<br>交渉中</button>
+          <button>会員<br>情報</button>
         </div>
       </div>
     </header>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//ユーザー関係のルート
+Route::get('/user/edit', 'UserController@edit');
+Route::post('/user/edit', 'UserController@brach')->name('user.branch');
