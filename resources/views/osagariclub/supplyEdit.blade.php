67833d1c1f01bd38260d94985f280748fc5fe6a5
tokie-inomata <asahane06@gmail.com>
2021-08-19 12:35:15 +0900

    おさがり登録編集画面の修正とJS追加

--- resources/views/osagariclub/supplyEdit.blade.php
+++ resources/views/osagariclub/supplyEdit.blade.php
@@ -2,74 +2,68 @@
 
 @section('title', 'おさがり情報編集')
 
+@section('js')
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+  @endsection
+
 @section('container')
   <div class="title-Container">
-    <h2>おさがり情報編集</h2>
+    <h2>おさがり情報を編集します。</h2>
   </div>
   <div class="supply-Edit-Container">
     <form action="{{route('supply.branch')}}" method="post" enctype="multipart/form-data">
       @csrf
-      <dl>
         <div class="form-Group">
-          <dt>おさがり名</dt>
-          <dd><input type="text" name="item" value="{{$search_supply['item']}}"><span>必須</span></dd>
+          <span>おさがり名＊</span>
+          <input type="text" name="item" value="{{$search_supply['item']}}">
         </div>
-        <div class="form-Group">
-          <dt>カテゴリー</dt>
-          <dd>
-            <select name="category_id">
-              <option disabled selected value>選択してください</option>
+        <div class="category-Group">
+          <span>カテゴリ＊</span>
+            <div class="category-Group-Button">
               @foreach($categories as $category)
-                <option value="{{$category['id']}}" {{$category['id'] == $search_supply['category_id'] ? 'selected' : ''}}>{{$category['category']}}</option>
+              <label><input type="radio" name="category_id" value="{{$category['id']}}" {{$search_supply['category_id'] == $category['id'] ? 'checked' : ''}}><span class="{{$search_supply['category_id'] == $category['id'] ? 'select-Category' : 'category'}}">{{$category['category']}}</span></label>
               @endforeach
-            </select>
-            <span>必須</span>
-          </dd>
+            </div>
         </div>
         <div class="form-Group">
-          <dt>サイズ</dt>
-          <dd><input type="text" name="size" value="{{$search_supply['size']}}"><span>必須</span></dd>
+          <span>サイズ＊</span>
+          <input type="text" name="size" value="{{$search_supply['size']}}">
         </div>
         <div class="form-Group">
-          <dt>使用年数</dt>
-          <dd><input type="text" name="years_used" value="{{$search_supply['years_used']}}"><span>必須</span></dd>
+          <span>使用年数＊</span>
+          <input type="text" name="years_used" value="{{$search_supply['years_used']}}">
         </div>
         <div class="form-Group gender-Group">
-          <dt>使用していた性別</dt>
-          <dd>
+          <span>使用していた性別＊</span>
             @foreach(config('const')['gender'] as $k => $val)
-              <input type="radio" name="gender" value="{{$k}}" {{$k == $search_supply['gender'] ? 'checked' : ''}}>{{$val}}
+              <label><span class="{{$k == $search_supply['gender'] ? 'select-Gender' : 'gender'}}"></span><input type="radio" name="gender" value="{{$k}}" {{$k == $search_supply['gender'] ? 'checked' : ''}}>{{$val}}</label>
             @endforeach
-          <span>必須</span>
-          </dd>
         </div>
-        <div class="form-Group condition-Group">
-          <dt>きれい度</dt>
-          <dd>
-            @foreach(config('const')['condition'] as $k => $val)
-              <ul>
-                <ol><input type="radio" name="condition" value="{{$k}}" {{$k == $search_supply['condition'] ? 'checked' : ''}}>{{$val}}</ol>
-              </ul>
-            @endforeach
-          <span>必須</span>
-          </dd>
+        <div class="condition-Group">
+          きれい度
+          <div class="condition-Group-Choice">
+              @foreach(config('const')['condition'] as $k => $val)
+                  <label><span class="{{$k == $search_supply['condition'] ? 'select-Condition' : 'condition'}}"></span><input type="radio" name="condition" value="{{$k}}" {{$k == $search_supply['condition'] ? 'checked' : ''}}><span class="color{{$k}}">{{$val}}</span></label>
+              @endforeach
+          </div>
         </div>
-        <div class="form-Group image-Group">
-          <dt>写真</dt>
-          <dd>
-            <input type="file" name="image_path1">
-            <input type="file" name="image_path2">
-            <input type="file" name="image_path3">
-            <input type="file" name="image_path4">
-          <span>１枚は必須</span>
-          </dd>
+        おさがりの写真
+        <div class="image-Group">
+          @for($n = 1; $n <= 4; $n++)
+            <label>
+              <div class="image{{$n}}">
+                <img id="img{{$n}}"><br>
+                <input type="file" name="image_path{{$n}}" class="imgFile{{$n}}">
+              </div>
+            </label>
+          @endfor
         </div>
         <div class="supply-Remarks-Container">
-          <dt>備考欄</dt>
-          <dd><textarea name="remarks" rows="4" cols="34">{{$search_supply['remarks']}}</textarea></dd>
+          備考
+          <textarea name="remarks" rows="5" cols="46%">{{$search_supply['remarks']}}</textarea>
         </div>
         <input type="hidden" name="id" value="{{$search_supply['id']}}">
-      </dl>
       <div class="btn-Group">
         <input type="submit" name="edit" value="確認する">
         @foreach($supply_user as $k => $val)
@@ -81,4 +75,6 @@
       </div>
     </form>
   </div>
+  <script src="{{ mix('js/app.js') }}"></script>
+  <script src="{{ asset('js/supplyForm.js') }}"></script>
 @endsection
