65c7ca6e19326cf2e6c81fcd6eba7f0addab289a
Yuuki-Tagaku <gyaaaaao.0v0.555@gmail.com>
2021-09-17 21:13:41 +0900

    test6

--- resources/views/osagariclub/userEdit.blade.php
+++ resources/views/osagariclub/userEdit.blade.php
@@ -4,7 +4,7 @@
 
 @section('js')
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-  <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 @endsection
 
 @section('container')
